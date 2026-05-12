2658624fd1b7340574ba72c73bb512dcf7e543c8
fazamumtaz <fazamumtazramadhan@gmail.com>
2026-05-12 12:34:12 +0700

    feat: press wallet/goal to detail

--- components/Portfolio/GoalCard.tsx
+++ components/Portfolio/GoalCard.tsx
@@ -1,6 +1,7 @@
 import useWalletTheme, { WalletThemeId } from "@/hooks/useWalletTheme";
+import { Link } from "expo-router";
 import React from "react";
-import { Text, View } from "react-native";
+import { Pressable, Text, View } from "react-native";
 
 const GoalCard = ({ goal, isBalanceShow, onBalanceShow }: any) => {
   const { theme } = useWalletTheme(goal.themeId as WalletThemeId);
@@ -13,72 +14,76 @@ const GoalCard = ({ goal, isBalanceShow, onBalanceShow }: any) => {
     return amount.toString();
   };
   return (
-    <View
-      key={goal.id}
-      style={{
-        shadowColor: "#000",
-        shadowOffset: {
-          width: 0,
-          height: 0,
-        },
-        shadowOpacity: 0.09,
-        shadowRadius: 3,
-        elevation: 5,
-        backgroundColor: theme.bgColor,
-      }}
-      className="w-[48%] p-4 relative rounded-3xl mb-5 flex justify-between"
-    >
-      {/* icon and name */}
-      <View style={{ gap: 5 }}>
-        <View>
-          <View className="flex flex-row items-start justify-between">
-            <View
-              style={{ backgroundColor: theme.iconBgColor }}
-              className="rounded-full justify-center items-center h-[48px] w-[48px]"
-            >
-              <Text className="text-lg">{goal.icon}</Text>
-            </View>
-            <Text
-              className="font-bold text-lg"
-              style={{ color: theme.accentColor }}
-            >
-              {`${percentage.toFixed(0)}%`}
-            </Text>
-          </View>
-          <Text className="text-lg font-bold mt-2">{goal.name}</Text>
-        </View>
-        {/* balance and bar */}
-        <View className="gap-1.5">
-          <View className="flex flex-row items-end">
-            <Text className="text-sm font-bold">
-              Rp{formatAmount(goal.current)}
-            </Text>
-            <Text className="text-xs font-semibold mt-2">
-              /Rp{formatAmount(goal.target)}
-            </Text>
-          </View>
+    <Link href={"/(others)/detailGoal"} asChild>
+      <Pressable
+        key={goal.id}
+        style={{
+          shadowColor: "#000",
+          shadowOffset: {
+            width: 0,
+            height: 0,
+          },
+          shadowOpacity: 0.09,
+          shadowRadius: 3,
+          elevation: 5,
+          backgroundColor: theme.bgColor,
+        }}
+        className="w-[48%] p-4 relative rounded-3xl mb-5 flex justify-between"
+      >
+        {/* icon and name */}
+        <View style={{ gap: 5 }}>
           <View>
-            <View
-              style={{
-                height: 5,
-                backgroundColor: "#fff",
-                borderRadius: 999,
-                overflow: "hidden",
-              }}
-            >
+            <View className="flex flex-row items-start justify-between">
+              <View
+                style={{ backgroundColor: theme.iconBgColor }}
+                className="rounded-full justify-center items-center h-[48px] w-[48px]"
+              >
+                <Text className="text-lg">{goal.icon}</Text>
+              </View>
+              <Text
+                className="font-bold text-lg"
+                style={{ color: theme.accentColor }}
+              >
+                {`${percentage.toFixed(0)}%`}
+              </Text>
+            </View>
+            <Text className="text-lg font-bold mt-2">{goal.name}</Text>
+          </View>
+          {/* balance and bar */}
+          <View className="gap-1.5">
+            <View className="flex flex-row items-end">
+              <Text className="text-sm font-bold">
+                Rp{formatAmount(goal.current)}
+              </Text>
+              <Text className="text-xs font-semibold mt-2">
+                /Rp{formatAmount(goal.target)}
+              </Text>
+            </View>
+            <View>
               <View
                 style={{
-                  width: `${percentage}%`,
-                  height: "100%",
+                  height: 5,
+                  backgroundColor: "#fff",
                   borderRadius: 999,
-                  backgroundColor: isCompleted ? "#00BC7D" : theme.accentColor,
+                  overflow: "hidden",
                 }}
-              />
+              >
+                <View
+                  style={{
+                    width: `${percentage}%`,
+                    height: "100%",
+                    borderRadius: 999,
+                    backgroundColor: isCompleted
+                      ? "#00BC7D"
+                      : theme.accentColor,
+                  }}
+                />
+              </View>
             </View>
           </View>
         </View>
-      </View>
-    </View>
+      </Pressable>
+    </Link>
   );
 };
 
--- components/Portfolio/WalletCard.tsx
+++ components/Portfolio/WalletCard.tsx
@@ -1,7 +1,8 @@
 import useWalletTheme, { WalletThemeId } from "@/hooks/useWalletTheme";
+import { Link } from "expo-router";
 import { WalletCards } from "lucide-react-native";
 import React from "react";
-import { Text, View } from "react-native";
+import { Pressable, Text, View } from "react-native";
 
 export interface WalletItem {
   id: string;
@@ -38,46 +39,48 @@ const WalletCard = ({
     return formatAmount(cleanNumber);
   };
   return (
-    <View
-      style={{
-        shadowColor: "#000",
-        shadowOffset: {
-          width: 0,
-          height: 0,
-        },
-        shadowOpacity: 0.04,
-        shadowRadius: 3,
-        elevation: 5,
-        backgroundColor: theme.cardColorBack,
-      }}
-      className="w-[48%] h-40 relative rounded-3xl mb-5"
-    >
-      <View className="flex-1 h-full items-center gap-1 py-2 m-3.5 rounded-2xl bg-white">
-        <View className="h-1 w-[85%] bg-gray-300 rounded-full"></View>
-        <View className="h-1 w-[85%] bg-gray-300 rounded-full"></View>
-        <View className="h-1 w-[50%] self-start ml-3 bg-gray-300 rounded-full"></View>
-      </View>
-      <View
-        style={{ backgroundColor: theme.cardColor }}
-        className="w-full h-[75%] absolute bottom-0 p-3 rounded-3xl flex justify-between"
+    <Link href={"/(others)/detailWallet"} asChild>
+      <Pressable
+        style={{
+          shadowColor: "#000",
+          shadowOffset: {
+            width: 0,
+            height: 0,
+          },
+          shadowOpacity: 0.04,
+          shadowRadius: 3,
+          elevation: 5,
+          backgroundColor: theme.cardColorBack,
+        }}
+        className="w-[48%] h-40 relative rounded-3xl mb-5"
       >
-        <View
-          style={{ backgroundColor: theme.cardColorBack }}
-          className="w-9 h-9 flex items-center justify-center rounded-lg"
-        >
-          <WalletCards size={20} color={"white"} />
+        <View className="flex-1 h-full items-center gap-1 py-2 m-3.5 rounded-2xl bg-white">
+          <View className="h-1 w-[85%] bg-gray-300 rounded-full"></View>
+          <View className="h-1 w-[85%] bg-gray-300 rounded-full"></View>
+          <View className="h-1 w-[50%] self-start ml-3 bg-gray-300 rounded-full"></View>
         </View>
-        <View className="gap-0.5`">
-          <Text className="text-white font-semibold">{item.bank}</Text>
-          <View className="flex flex-row items-end gap-0.5">
-            <Text className="text-white font-semibold text-sm mb-1">Rp</Text>
-            <Text className="text-white font-bold text-lg">
-              {isBalanceShow ? item.balance : "•••••••••"}
-            </Text>
+        <View
+          style={{ backgroundColor: theme.cardColor }}
+          className="w-full h-[75%] absolute bottom-0 p-3 rounded-3xl flex justify-between"
+        >
+          <View
+            style={{ backgroundColor: theme.cardColorBack }}
+            className="w-9 h-9 flex items-center justify-center rounded-lg"
+          >
+            <WalletCards size={20} color={"white"} />
+          </View>
+          <View className="gap-0.5`">
+            <Text className="text-white font-semibold">{item.bank}</Text>
+            <View className="flex flex-row items-end gap-0.5">
+              <Text className="text-white font-semibold text-sm mb-1">Rp</Text>
+              <Text className="text-white font-bold text-lg">
+                {isBalanceShow ? item.balance : "•••••••••"}
+              </Text>
+            </View>
           </View>
         </View>
-      </View>
-    </View>
+      </Pressable>
+    </Link>
   );
 };
 
